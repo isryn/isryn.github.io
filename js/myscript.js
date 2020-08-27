1749c0e8f6a1edec61bc52b2384c35b31f0c9ec4
isryn <piksi.isryn.18402051@gmail.com>
2020-08-27 20:24:36 +0700

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+$( document ).ready(function(){
+	$(".button-collapse").sideNav();
+	$('.slider').slider({
+	});
+	$('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+	$('select').material_select();
+	$('input#input_text, textarea#textarea1').characterCounter();
+});
